e362cd59c1af94fef8fe95aa59eddcee48a1a907
abelnyeumro <abelnijeumro@gmail.com>
2024-10-11 14:14:30 +0200

    Add UI

--- rps-js.js
+++ rps-js.js
@@ -15,64 +15,113 @@ function getComputerChoice() {
     return choice;
 }
 
-function getHumanChoice() {
-    let choice = prompt("Please enter either rock/paper/scissors").toLowerCase();
-    return choice;
-}
 
 let humanScore = 0;
 let cpuScore = 0;
 
-function playRound(humanChoice,cpuChoice) {
-    console.log('Human choice: ' + humanChoice);
-    console.log('Cpu choice: ' + cpuChoice);
-    if(humanChoice == 'rock' && cpuChoice == 'paper'){
-        console.log('You lose! Paper beats rock.');
-        cpuScore++
-    }
-    else if(humanChoice == 'paper' && cpuChoice == 'rock'){
-        console.log('You win! Paper beats rock.');
-        humanScore++
-    }
-    else if(humanChoice == 'rock' && cpuChoice == 'scissors'){
-        console.log('You win! Rock beats scissors.');
-        humanScore++
-    }
-    else if(humanChoice == 'scissors' && cpuChoice == 'rock'){
-        console.log('You lose! Rock beats scissors.');
-        cpuScore++
-    }
-    else if(humanChoice == 'paper' && cpuChoice == 'scissors'){
-        console.log('You lose! Scissors beats paper.');
-        cpuScore++
-    }
-    else if(humanChoice == 'scissors' && cpuChoice == 'paper'){
-        console.log('You win! Scissors beats paper.');
-        humanScore++
-    }
-    else if(humanChoice == 'scissors' && cpuChoice == 'scissors'){
-        console.log('You both choose scissors. Nobody wins.');
-    }
-    else if(humanChoice == 'rock' && cpuChoice == 'rock'){
-        console.log('You both choose rock. Nobody wins.');
-    }
-    else if(humanChoice == 'paper' && cpuChoice == 'paper'){
-        console.log('You both choose paper. Nobody wins.');
-    }
-}
 
-function playGame() {
-    for(let i= 1; i <= 5; i++){
-        const humanSelection = getHumanChoice();
+function playGame(choice) {
+        const humanSelection = choice;
         const computerSelection = getComputerChoice();
+        console.log('Human choice: ' + humanSelection);
+        console.log('Cpu choice: ' + computerSelection);
+        
+        if(humanScore === 5) return "You win!";
+        else if(cpuScore === 5){
+            return "YOu lose!";
+        }
 
-        playRound(humanSelection, computerSelection);
+        if(humanSelection == 'rock' && computerSelection == 'paper'){
+            cpuScore++;
+            let result =  'You lose! Paper beats rock.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore; 
+                resultsDiv.style.backgroundColor = 'red';
+            return result;
+        }
+        else if(humanSelection == 'paper' && computerSelection == 'rock'){
+            humanScore++;
+            let result =  'You win! Paper beats rock.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore; 
+                resultsDiv.style.backgroundColor = 'green';
+            return result;
+        }
+        else if(humanSelection == 'rock' && computerSelection == 'scissors'){
+            humanScore++;
+            let result =  'You win! Rock beats scissors.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'green'; 
+            return result;
+        }
+        else if(humanSelection == 'scissors' && computerSelection == 'rock'){
+            cpuScore++;
+            let result = 'You lose! Rock beats scissors.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore; 
+                resultsDiv.style.backgroundColor = 'red';
+            return result;
+        }
+        else if(humanSelection == 'paper' && computerSelection == 'scissors'){
+            cpuScore++;
+            let result = 'You lose! Scissors beats paper.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'red';
+            return result;
+        }
+        else if(humanSelection == 'scissors' && computerSelection == 'paper'){
+            humanScore++;
+            let result =  'You win! Scissors beats paper.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'green';
+            return result;
+        }
+        else if(humanSelection == 'scissors' && computerSelection == 'scissors'){
+            let result =  'You both choose scissors. Nobody wins.'+
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'black';
+            return result;
+        }
+        else if(humanSelection == 'rock' && computerSelection == 'rock'){
+            let result =  'You both choose rock. Nobody wins.' +
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'black';
+            return result;
+        }
+        else if(humanSelection == 'paper' && computerSelection == 'paper'){
+            let result =  'You both choose paper. Nobody wins.'+
+                ' Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore;
+                resultsDiv.style.backgroundColor = 'black';
+            return result;
+        }
 
-        console.log('Human score : ' + humanScore + ' ' +'Cpu score : ' + cpuScore);
-    }
 }
 
-playGame();
 
 
+const rockButton = document.querySelector("#rock");
+rockButton.textContent = "Rock";
+
+
+const paperButton = document.querySelector("#paper");
+paperButton.textContent = "Paper";
+
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.textContent = "Scissors";
+
+
+const resultsDiv = document.querySelector(".resultDiv");
+
+rockButton.addEventListener("click", () => {
+    let result = playGame("rock");
+    resultsDiv.textContent = result;
+});
+
+paperButton.addEventListener("click", () => {
+    let result = playGame("paper");
+    resultsDiv.textContent = result;
+});
+
+scissorsButton.addEventListener("click", () => {
+    let result = playGame("scissors");
+    resultsDiv.textContent = result;
+});
 
